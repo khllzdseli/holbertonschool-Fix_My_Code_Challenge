b5b5013ce00b43c310069e1a864cd7c8512907c9
khllzdseli <princesskhalilzadeh@gmail.com>
2026-06-10 22:45:23 +0400

    Update 0-fizzbuzz.py

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -4,15 +4,15 @@
 
 def fizzbuzz():
     """Print numbers from 1 to 100 with FizzBuzz rules"""
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
-        elif i % 3 == 0:
-            print("Fizz", end=" ")
-        elif i % 5 == 0:
-            print("Buzz", end=" ")
-        else:
-            print(i, end=" ")
+for i in range(1, 90):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz", end=" ")
+    elif i % 3 == 0:
+        print("Fizz", end=" ")
+    elif i % 5 == 0:
+        print("Buzz", end=" ")
+    else:
+        print(i, end=" ")
 
 
 if __name__ == "__main__":

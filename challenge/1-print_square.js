5d5ad205c0e7c1461faa4caf394569582a515e62
khllzdseli <princesskhalilzadeh@gmail.com>
2026-06-13 22:07:52 +0400

    Update 1-print_square.js

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -1,15 +1,22 @@
-#!/usr/bin/node
+#!/usr/bin/env node
+/*
+ * Prints a square with the character #
+ */
+if (process.argv.length <= 2) {
+    process.stderr.write("Missing argument\n");
+    process.exit(1);
+}
 
-const size = parseInt(process.argv[2]);
-
+const size = parseInt(process.argv[2], 10);
 if (isNaN(size)) {
-  console.log('Missing size');
-} else {
-  for (let i = 0; i < size; i++) {
-    let line = '';
+    process.stderr.write("Argument must be a number\n");
+    process.exit(1);
+}
+
+for (let i = 0; i < size; i++) {
+    let row = "";
     for (let j = 0; j < size; j++) {
-      line += '#';
+        row += "#";
     }
-    console.log(line);
-  }
+    console.log(row);
 }

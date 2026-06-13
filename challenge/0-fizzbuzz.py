e5e67bdd3bb38bb15e7839eb570c4b427c7e2fbe
khllzdseli <princesskhalilzadeh@gmail.com>
2026-06-13 22:08:19 +0400

    Update 0-fizzbuzz.py

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,19 +1,26 @@
-#!/usr/bin/python3
-"""FizzBuzz"""
+#!/usr/bin/env python3
+"""
+FizzBuzz Implementation - Fixed logical order for multiples of both 3 and 5
+"""
+import sys
 
-
-def fizzbuzz():
-    """Print numbers from 1 to 100 with FizzBuzz rules"""
-for i in range(1, 90):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz", end=" ")
-    elif i % 3 == 0:
-        print("Fizz", end=" ")
-    elif i % 5 == 0:
-        print("Buzz", end=" ")
-    else:
-        print(i, end=" ")
-
+def fizzbuzz(n):
+    if n < 1: return
+    output = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0: output.append("FizzBuzz")
+        elif i % 3 == 0: output.append("Fizz")
+        elif i % 5 == 0: output.append("Buzz")
+        else: output.append(str(i))
+    print(" ".join(output))
 
 if __name__ == "__main__":
-    fizzbuzz()
+    if len(sys.argv) <= 1:
+        print("Missing argument")
+        sys.exit(1)
+    try:
+        number = int(sys.argv[1])
+        fizzbuzz(number)
+    except ValueError:
+        print("Argument must be an integer")
+        sys.exit(1)

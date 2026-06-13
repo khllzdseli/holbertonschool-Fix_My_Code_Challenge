31519efa8151c9f49e9ee70cf5ef8ecbb0be859c
khllzdseli <princesskhalilzadeh@gmail.com>
2026-06-13 22:04:41 +0400

    Update delete_dnodeint_at_index.c

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,46 +1,22 @@
-#include "lists.h"
 #include <stdlib.h>
-
+#include "lists.h"
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current;
-    dlistint_t *tmp;
+    dlistint_t *saved_head = *head;
     unsigned int i = 0;
-
-    if (head == NULL || *head == NULL)
-        return (-1);
-
-    current = *head;
-
-    /* delete head */
-    if (index == 0)
+    if (*head == NULL) return (-1);
+    while (saved_head != NULL && i < index) { saved_head = saved_head->next; i++; }
+    if (saved_head == NULL) return (-1);
+    if (saved_head == *head)
     {
-        tmp = current;
-        *head = current->next;
-
-        if (*head != NULL)
-            (*head)->prev = NULL;
-
-        free(tmp);
-        return (1);
+        *head = saved_head->next;
+        if (*head != NULL) (*head)->prev = NULL;
     }
-
-    while (current != NULL && i < index)
+    else
     {
-        current = current->next;
-        i++;
+        saved_head->prev->next = saved_head->next;
+        if (saved_head->next != NULL) saved_head->next->prev = saved_head->prev;
     }
-
-    if (current == NULL)
-        return (-1);
-
-    if (current->prev != NULL)
-        current->prev->next = current->next;
-
-    if (current->next != NULL)
-        current->next->prev = current->prev;
-
-    free(current);
-
+    free(saved_head);
     return (1);
 }
